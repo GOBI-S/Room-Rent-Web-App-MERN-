6b36fe1696e9e9a4158b79414532c26de1e5140b
GOBI-S <gobidreamer@gmail.com>
2025-02-11 02:08:17 +0530

    jwt hide finsihed product

--- backend/src/Controllers/jwttokenverify.ts
+++ backend/src/Controllers/jwttokenverify.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const secretKey = "gobi";
+const secretKey = process.env.JWT_SECRET_KEY;
 
 // Extending the Request type to include 'user'
 interface AuthRequest extends Request {
@@ -16,6 +16,9 @@ const   tokenverify = async (req: AuthRequest, res: Response, next: NextFunction
   }
 else{
   try {
+    if (!secretKey) {
+      throw new Error("Missing JWT secret key. Please set JWT_SECRET_KEY in your environment variables.");
+    }
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded; // Attach decoded user data to the request
     next(); // Proceed to the next middleware or route handler
